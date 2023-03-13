b9b823553297c09c4eb1ab1ac1314cf0d4c29758
grwadl <youngwwad@gmail.com>
2023-03-13 21:09:38 +0200

    feat(categories): new route categories

---
+++ frontend/modules/ad/mocks/categories.ts
@@ -0,0 +1,15 @@
+import { Subject } from "@/models/category";
+
+export const initialData = {
+  [Subject.Physics]: 0,
+  [Subject.IT]: 0,
+  [Subject.Chemistry]: 0,
+  [Subject.Math]: 0,
+};
+
+export const subjectMapping: Record<number, string> = {
+  0: Subject.Physics,
+  1: Subject.IT,
+  2: Subject.Chemistry,
+  3: Subject.Math,
+};
