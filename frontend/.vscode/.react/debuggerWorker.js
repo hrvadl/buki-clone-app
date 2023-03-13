2b1c0526e700c8ed5e8815548973735d185a6fa8
grwadl <youngwwad@gmail.com>
2023-03-13 21:07:38 +0200

    feat(frontend): * configured debugger

--- frontend/.vscode/.react/debuggerWorker.js
+++ frontend/.vscode/.react/debuggerWorker.js
@@ -129,11 +129,10 @@ Object.prototype.toString = function() {
  */
 
 /* global __fbBatchedBridge, self, importScripts, postMessage, onmessage: true */
-
 /* eslint no-unused-vars: 0 */
+
 onmessage = function () {
   var visibilityState;
-
   var showVisibilityWarning = function () {
     var hasWarned = false;
     return function () {
@@ -141,29 +140,22 @@ onmessage = function () {
       if (hasWarned || console.warn.toString().includes('[native code]')) {
         return;
       }
-
       hasWarned = true;
       console.warn('Remote debugger is in a background tab which may cause apps to ' + 'perform slowly. Fix this by foregrounding the tab (or opening it in ' + 'a separate window).');
     };
   }();
-
   var messageHandlers = {
     executeApplicationScript: function (message, sendReply) {
       for (var key in message.inject) {
         self[key] = JSON.parse(message.inject[key]);
       }
-
       var error;
-
       try {
         importScripts(message.url);
       } catch (err) {
         error = err.message;
       }
-
-      sendReply(null
-      /* result */
-      , error);
+      sendReply(null /* result */, error);
     },
     setDebuggerVisibility: function (message) {
       visibilityState = message.visibilityState;
@@ -173,9 +165,7 @@ onmessage = function () {
     if (visibilityState === 'hidden') {
       showVisibilityWarning();
     }
-
     var object = message.data;
-
     var sendReply = function (result, error) {
       postMessage({
         replyID: object.id,
@@ -183,9 +173,7 @@ onmessage = function () {
         error: error
       });
     };
-
     var handler = messageHandlers[object.method];
-
     if (handler) {
       // Special cased handlers
       handler(object, sendReply);
@@ -193,7 +181,6 @@ onmessage = function () {
       // Other methods get called on the bridge
       var returnValue = [[], [], [], 0];
       var error;
-
       try {
         if (typeof __fbBatchedBridge === 'object') {
           returnValue = __fbBatchedBridge[object.method].apply(null, object.arguments);
